aba2fffb8f0951e47d21d8b67fdb7002fa9517ee
M4LW0R3 <youngkhalifah98@gmail.com>
2021-06-01 19:27:09 +0300

    secret key obsfucated

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,8 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--zz2*=m_^#l@9g1mp!18(#81-e0i(o$fy^4qg^kos0n1i_w0j9'
-
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
