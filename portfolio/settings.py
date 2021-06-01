d12aef8f981e325a3dd8ee9a459f35a18667eac5
M4LW0R3 <youngkhalifah98@gmail.com>
2021-06-01 18:40:59 +0300

    malwore app configurations done

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'malwore',
 ]
 
 MIDDLEWARE = [
@@ -118,6 +119,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR,'media_root/')
+STATIC_ROOT = os.path,join(BASE_DIR, 'static_root/')
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
